64b2f0bdf9d88f44d53163c43a8f16d0c5156dc3
Keaton Freed <keaton@mfreed.com>
2024-04-28 14:57:41 -0700

    add to sidebar styles and patch

--- src/content.js
+++ src/content.js
@@ -1,3 +1,29 @@
 
 console.log("Slides Animator Pro: Loaded")
 
+document.querySelector("div")
+
+const motionPanelCallback = (mutations, observer) => {
+    for (mutation of mutations) {
+        if (mutation.type === "childList" && mutation.addedNodes?.length) {
+            let added = Array.from(mutation.addedNodes)
+            let sidebar = added?.find((el) => el.classList.contains("punch-animation-sidebar"))
+            if (sidebar) {
+                console.log("Found Motion Pane", sidebar)
+                sidebarPatch(sidebar)
+            }
+        }
+    }
+}
+
+const motionPanelConfig = { attributes: false, childList: true, subtree: false };
+
+const motionPanelObserver = new MutationObserver(motionPanelCallback);
+
+motionPanelObserver.observe(document.body, motionPanelConfig)
+
+
+function sidebarPatch(sidebar) {
+    let title = sidebar.querySelector("#punch-animation-sidebar-title")
+    title.textContent = "Animator Pro"
+}
