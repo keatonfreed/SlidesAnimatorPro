379790f3a1fc426d9541164f0764788e6c26b8c3
Keaton Freed <keatonafreed2@icloud.com>
2024-04-28 17:22:31 -0700

    work on auto open transitions

--- src/content.js
+++ src/content.js
@@ -22,6 +22,40 @@ const motionPanelObserver = new MutationObserver(motionPanelCallback);
 
 motionPanelObserver.observe(document.body, motionPanelConfig)
 
+function openSidebar() {
+    const sidebarShortcutMac = {
+        bubbles: true,
+        key: "B",
+        keyCode: 66,
+        which: 66,
+        code: "KeyB",
+        location: 0,
+        altKey: true,
+        ctrlKey: false,
+        metaKey: true,
+        shiftKey: true,
+        repeat: false
+    };
+    const sidebarShortcut = {
+        bubbles: true,
+        key: "B",
+        keyCode: 66,
+        which: 66,
+        code: "KeyB",
+        location: 0,
+        altKey: true,
+        ctrlKey: true,
+        metaKey: false,
+        shiftKey: true,
+        repeat: false
+    }
+    const osBasedEvent = navigator.userAgent.includes('Mac') ? sidebarShortcutMac : sidebarShortcut
+    document.querySelector("#docs-menubar").dispatchEvent(new MouseEvent("keydown", osBasedEvent))
+    document.querySelector("#docs-menubar").dispatchEvent(new MouseEvent("keyup", osBasedEvent))
+    console.log("sending open command for sidebar", osBasedEvent, navigator.userAgent.includes('Mac'), sidebarShortcutMac, sidebarShortcut)
+}
+
+setTimeout(openSidebar, 5000)
 
 function sidebarPatch(sidebar) {
     let title = sidebar.querySelector("#punch-animation-sidebar-title")
